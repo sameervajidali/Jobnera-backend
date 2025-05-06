b65e6b56317db49b940e4c5ea3ad13bc136a1ad9
VAJID ALI <sameervajidali@gmail.com>
2025-05-06 22:21:01 +0530

    Profile update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -79,6 +79,8 @@ export const activateAccount = asyncHandler(async (req, res) => {
 export const login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
+  console.log("Received login request with:", { email, password });
+
   // Check if user exists
   const user = await User.findOne({ email }).select('+password');
   if (!user || !(await bcrypt.compare(password, user.password))) {
